d1745f235729b2a63dcdab37b02e931036cc0dcb
VeeTk <mr.felisity@mail.ru>
2023-10-10 12:36:32 +0300

    Продолжаю написание JS для элемента страницы 'see all'

--- js/seeAllBtn.js
+++ js/seeAllBtn.js
@@ -5,9 +5,13 @@ const box5 = document.querySelector(".boxes__box-5");
 const box6 = document.querySelector(".boxes__box-6");
 const seeAllActive = document.querySelector(".recomendation__boxes");
 
-seeAllBtn.addEventListener('click', () => {
-  seeAllActive.classList.toggle("active-seeAll");
-  box4.classList.toggle('hover');
-  box5.classList.toggle('hover');
+seeAllBtn.addEventListener("click", () => {
+  if (seeAllActive.style.flexWrap === "wrap") {
+    seeAllActive.style.flexWrap = "no-wrap";
+  } else {
+    seeAllActive.style.flexWrap = "wrap";
+  }
+  box4.classList.toggle("hover");
+  box5.classList.toggle("hover");
   box6.classList.toggle("hover");
-})
+});
