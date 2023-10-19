5f29fe749dafda309f56f02ae64da2ab6f928bdd
VeeTk <mr.felisity@mail.ru>
2023-10-19 20:24:29 +0300

    Проведена работа над адаптивностью страницы, и над скриптами
    Страница адаптивна на всем поле размеров экрана

--- js/active-img.js
+++ js/active-img.js
@@ -1,8 +1,28 @@
 const recomendationBtn = document.querySelectorAll(".box-style");
 const activeImg = document.querySelector(".summary__active-img");
 
+const activeTitle = document.querySelector(".active-img__titles-title");
+const activeSubTitle = document.querySelector(".active-img__titles-subtitle");
+
+const title = document.querySelectorAll(".heading");
+const subTitle = document.querySelectorAll(".subheading");
+
 recomendationBtn.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     activeImg.style.backgroundImage = btn.style.backgroundImage;
+    // activationTitle();
+
+    title.forEach((title, index) => {
+      console.log(index);
+    });
+    // console.log(btn.parentElement[index].innerText);
+
+    // activationTitle();
+    // activeTitle.innerText = title.innerText;
   });
 });
+function activationTitle() {
+  title.forEach((title, index) => {
+    console.log(title.innerText);
+  });
+}
